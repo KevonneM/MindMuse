47f1a5904d9df38e097ac92a086d79e8ae1721a1
Kevonne Cinque Montes <Kevonnem@gmail.com>
2023-04-17 05:55:35 +0000

    Added sign out and simple navigation bar to be customized in the future.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # logs the user in after signup
+            login(request, user)
+            return redirect('second_brain:home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
