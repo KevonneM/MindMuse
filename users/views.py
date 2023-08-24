0fa2f7f4647c4582cdb8e686dcf76dc2188aab95
Kevonne Cinque Montes <Kevonnem@gmail.com>
2023-08-24 05:07:05 +0000

    Added custom login view and edited custom sign-in view to handle request from modal to redirect users to hub.

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import JsonResponse
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,7 +14,35 @@ def signup(request):
             user = form.save()
             # logs the user in after signup
             login(request, user)
+
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return JsonResponse({'success': 'ok', 'redirect_url': reverse('second_brain:home')})
             return redirect('second_brain:home')
     else:
         form = CustomUserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+
+    if 'HTTP_X_REQUESTED_WITH' in request.META and request.META['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest':
+        return render(request, 'registration/_signup.html', {'form': form})
+    else:
+        return render(request, 'registration/signup.html', {'form': form})
+
+def custom_login_view(request):
+    if request.method == 'POST':
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return JsonResponse({'status': 'ok', 'redirect_url': reverse('second_brain:home')})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Invalid credentials'})
+        else:
+            return redirect('second_brain:home')
+    else:
+        form = AuthenticationForm()
+        # If it's an AJAX request, render only the login form, otherwise render the whole page.
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return render(request, 'registration/_login.html', {'form': form})
+        else:
+            return render(request, 'registration/login.html', {'form': form})
